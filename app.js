a95cf05d981e99fa402b454db83a49a41cf869bc
YOON, sangmin <mis93ysm@daum.net>
2019-02-04 13:54:59 +0900

    second

---
+++ app.js
@@ -0,0 +1,33 @@
+
+import express from 'express';
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+import routes from "./routes";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+
+
+const app = express();
+
+const handleHome = (req, res) => res.send("Hello from Es6 arrow function ");
+  
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+  
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
+
+
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
+  
+export default app;
