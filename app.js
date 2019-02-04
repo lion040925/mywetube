e6912e2bb85a91ba59bb1a7be998e2adef22acd2
YOON, sangmin <mis93ysm@daum.net>
2019-02-04 16:37:52 +0900

    2-16

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
 
 import routes from "./routes";
 import userRouter from "./routers/userRouter";
@@ -23,8 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
-
-
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
